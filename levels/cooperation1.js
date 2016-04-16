400959d945ee29d812d3c76f442f746ca3f9f3b7
csimstu <csimstu@DN51q05n.SUNet>
2016-04-16 00:47:15 -0700

    Add cooperation level 1 & template

---
+++ levels/cooperation1.js
@@ -0,0 +1,66 @@
+(function () {
+var level = 
+  {
+    // Level name
+    name   : 'Cooperation 1',
+    // Width of level in tiles
+    width  : 5,
+    // Height of level in tiles
+    height : 5,
+    // Player properties
+    frog : {
+      // Player's initial x position
+      x : 2,
+      // Player's initial y position
+      y : 2,
+      // Range for player's tongue, computed using Manhattan metric
+      range : 0,
+    },
+    // List of flies, see below for example fly.
+    flies : [],
+    // List of walls, see below for example wall.
+    walls: [],
+    // Objective function, takes in number of flies eaten and moves made
+    scoreFunction : function (flies, moves) {
+      return moves - flies;
+    }
+  };
+
+var fliesInfo = [
+  [1,1,'D'],
+  [3,1,'U'],
+  [3,3,'L'],
+  [1,3,'R'],
+];
+
+for (var i = 0; i < fliesInfo.length; i++) {
+  var cur = fliesInfo[i];
+  level.flies.push({
+    x: cur[0], 
+    y: cur[1], 
+    hasMoved: i == 0 ? true : false, // need to expose the first fly
+    move : (function(timeToDie, dirChar) {
+      return function(level) {
+        if (level.flies.length == timeToDie && this.hasMoved == false) {
+          this.hasMoved = true;
+          if (dirChar == 'D') {
+            return {x : this.x, y : this.y - 1};
+          } else if (dirChar == 'U') {
+            return {x : this.x, y : this.y + 1};
+          } else if (dirChar == 'L') {
+            return {x : this.x - 1, y : this.y};
+          } else {
+            return {x : this.x + 1, y : this.y};
+          }
+        }
+        return {x : this.x, y : this.y}
+      }
+    })(fliesInfo.length - i, cur[2])
+  });
+
+  if (i > 0)
+    level.walls.push({x : cur[0], y : cur[1]});
+}
+
+game.levels.push(level);
+})();
