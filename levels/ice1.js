6d34d00154aa537dfb1a011cc9c6656e0e368b34
Herman Chau <hchau@stanford.edu>
2016-04-20 18:43:04 -0700

    Update ice level 1.

--- levels/ice1.js
+++ levels/ice1.js
@@ -4,13 +4,13 @@
 /***************************/
 var ice = 
   {
-    name   : 'Ice Puzzle',
-    width  : 17,
-    height : 12,
+    name   : 'Ice Zone 1',
+    width  : 16,
+    height : 14,
     frog : {
-      x : 1,
-      y : 1,
-      range : 1,
+      x : 14,
+      y : 12,
+      range : 0,
       moveFrog : function (level, dx, dy) {
         while (!level.walls[level.frog.y+dy][level.frog.x+dx]) {
           level.frog.x += dx;
@@ -18,15 +18,16 @@ var ice =
         }
       }
     },
-    flies : [{x:11, y:4, move: function() {return {x:this.x, y:this.y};}}],
+    flies : [{x:1, y:4, move: function() {return {x:this.x, y:this.y};}},
+             {x:14, y:8, move: function() {return {x:this.x, y:this.y};}}],
     walls: [
-      {x:6,y:1}, {x:7,y:1}, {x:4,y:3}, {x:9,y:3},
-      {x:11,y:3}, {x:12,y:3}, {x:15,y:3}, {x:12,y:4},
-      {x:3,y:5}, {x:8,y:5}, {x:8,y:6}, {x:13,y:6},
-      {x:5,y:7}, {x:14,y:8}, {x:15,y:9}
+      {x:9,y:1}, {x:4,y:2}, {x:10,y:3}, {x:2,y:4},
+      {x:1,y:5}, {x:9,y:5}, {x:14,y:6}, {x:7,y:7},
+      {x:3,y:8}, {x:14,y:9}, {x:8,y:10}, {x:6,y:11},
+      {x:10,y:11}
     ],
     scoreFunction : function (flies, moves) {
-      return flies/(moves+1);
+      return 20*(flies*flies)/(moves+1);
     }
   }
 
