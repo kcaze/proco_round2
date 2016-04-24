0bf819cac985735f67a43d84d0352cb8c7e842b5
csimstu <csimstu@DN0a2467f7.SUNet>
2016-04-24 14:11:50 -0700

    Finishing the randomized generation of cooperation level

--- levels/cooperation1.js
+++ levels/cooperation1.js
@@ -1,12 +1,16 @@
 (function () {
+  function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+  }
+
 var level = 
   {
     // Level name
     name   : 'Cooperation 1',
     // Width of level in tiles
-    width  : 5,
+    width  : 12,
     // Height of level in tiles
-    height : 5,
+    height : 12,
     // Player properties
     frog : {
       // Player's initial x position
@@ -22,16 +26,54 @@ var level =
     walls: [],
     // Objective function, takes in number of flies eaten and moves made
     scoreFunction : function (flies, moves) {
-      return moves - flies;
-    }
+      return Math.exp((2 * flies * flies - moves) / 100.);
+    },
   };
 
-var fliesInfo = [
-  [1,1,'D'],
-  [3,1,'U'],
-  [3,3,'L'],
-  [1,3,'R'],
-];
+var genFly = function() {
+  while (true) {
+    var x = getRandomInt(0, level.width);
+    var y = getRandomInt(0, level.height);
+    var dir = getRandomInt(0, 4);
+    if (dir == 0) {
+      if (y + 1 < level.height) return [x, y, 'U'];
+    } else if (dir == 1) {
+      if (x + 1 < level.width) return [x, y, 'R'];
+    } else if (dir == 2) {
+      if (y - 1 >= 0) return [x, y, 'D'];
+    } else {
+      if (x - 1 >= 0) return [x, y, 'L'];
+    }
+  }
+};
+
+var walk = function(f) {
+  if (f[2] == 'U') return [f[0], f[1] + 1];
+  if (f[2] == 'D') return [f[0], f[1] - 1];
+  if (f[2] == 'L') return [f[0] - 1, f[1]];
+  if (f[2] == 'R') return [f[0] + 1, f[1]];
+};
+
+var fliesInfo = [];
+for (var i = 0; i < 20; i++) {
+  var fly;
+  do {
+    fly = genFly();
+  } while ((function(x, y){
+    for (var k = 0; k < fliesInfo.length; k++) {
+      if (fliesInfo[k][0] == x && fliesInfo[k][1] == y)
+        return true;
+      var tmp = walk(fly);
+      if (fliesInfo[k][0] == tmp[0] && fliesInfo[k][1] == tmp[1])
+        return true;
+      tmp = walk(fliesInfo[k]);
+      if (tmp[0] == fly[0] && tmp[1] == fly[1])
+        return true;
+    }
+    return false;
+  })(fly[0], fly[1]));
+  fliesInfo.push(fly);
+}
 
 for (var i = 0; i < fliesInfo.length; i++) {
   var cur = fliesInfo[i];
